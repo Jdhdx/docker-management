856a360fb85c52335a3dba22128704ccbf6b3f51
silentreaper <wagdymichael92@gmail.com>
2026-03-23 18:38:53 +0200

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -63,22 +63,7 @@ while true;do
 		0)
 			main_menu;;
 		00)
-			sudo apt-get install \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-sudo usermod -aG docker $USER
-sudo mkdir -p /mnt/srv/{docker/{comp,cont},data}
-sudo chown -R $USER:$USER /mnt/srv/{docker,data}
-
+			curl -fsSL https://get.docker.com | sh
 ;;
 		1)
 		docker_run;;
